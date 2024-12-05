01d3a7be25c2630e1475fbb149118a8d89f0e03f
Alejo Perea <mpereamejia@gmail.com>
2024-12-05 14:51:10 -0500

    Creacion clases AlertService

---
+++ AlertService.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.JSInterop;
+
+namespace LabScript
+{
+	public class AlertService : IAsyncDisposable
+	{
+		readonly Lazy<Task<IJSObjectReference>> jSObjectReference;
+
+		public AlertService (IJSRuntime ijsRuntime)
+		{
+			this.jSObjectReference = new Lazy<Task<IJSObjectReference>>(()=>
+			ijsRuntime.InvokeAsync<IJSObjectReference>("import","./content/LabScript/Pages/Home.razor.js").AsTask());
+		}
+		public ValueTask DisposeAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
